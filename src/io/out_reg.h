1bf0876aca4542fe312142fd18090f04e46327a1
Igor Ochocki <unigniu14@gmail.com>
2024-04-14 11:59:00 +0200

    Implemented printing path

---
+++ src/io/out_reg.h
@@ -0,0 +1,11 @@
+#ifndef _OUT_REG_H_
+#define _OUT_REH_H_
+
+#include <stdio.h>
+#include "../maze.h"
+#include "coder.h"
+
+void reverse_path(FILE *data, struct maze *m);
+void print_path(FILE *data, struct maze *m);
+
+#endif
